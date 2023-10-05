e93eaf8cadf9d5d8a5d432ef66351763a5faea0c
samxchen <samxchen@tencent.com>
2023-10-05 23:53:52 +0800

    init

--- packages/plugin/src/service.ts
+++ packages/plugin/src/service.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import * as http from 'http';
 
 import { server as tsServer, Node } from 'typescript/lib/tsserverlibrary';
-import 'open-typescript';
 
 import * as ts from 'typescript';
 
@@ -34,22 +33,29 @@ function createApp(options: { info: tsServer.PluginCreateInfo }) {
   app.post('/get-type', (req, res) => {
     async function handle() {
       try {
+        logger.info(`[TS-Faker][File-Name] ${req.body.fileName}, ${req.body.position}`);
+
         const program = info.languageService.getProgram();
 
         const typeChecker = program?.getTypeChecker();
+        // @FIXME: getSourceFile sometimes return undefined
         const sourceFile = program?.getSourceFile(req.body.fileName);
+        if (!sourceFile) {
+          throw new Error('sourceFile not found');
+        }
+        logger.info(`[TS-Faker][Source-File] ${sourceFile.fileName}`);
 
-        logger.info(`[TS-Faker][File-Name] ${req.body.fileName}, ${req.body.position}`);
+        const node = findNode(sourceFile.getChildren(), req.body.position);
+        if (!node) {
+          throw new Error('node not found');
+        }
+        logger.info(`[TS-Faker][Node] ${node.getText()}`);
 
-        const node = ts.getTokenAtPosition(sourceFile as ts.SourceFile, req.body.position);
-
-        let type = typeChecker?.getTypeAtLocation(node as Node);
-        // @why 实测 language service 返回的 type 有可能不准，会变成 any
-        // @how 尝试 5 次，如果都是 any，就放过
+        let type = typeChecker?.getTypeAtLocation(node);
         let tryCount = 1;
-        while (((type?.flags ?? 0) & ts.TypeFlags.Any) === 0 && tryCount < 5) {
+        while (((type?.flags ?? 0) & ts.TypeFlags.Any) === 0 && tryCount < 3) {
           await new Promise((resolve) => setTimeout(resolve, 50 * tryCount));
-          type = typeChecker?.getTypeAtLocation(node as Node);
+          type = typeChecker?.getTypeAtLocation(node);
           tryCount++;
         }
 
@@ -68,6 +74,7 @@ function createApp(options: { info: tsServer.PluginCreateInfo }) {
         );
       } catch (err) {
         logger.info(`[TS-Faker] Error: ${err as any}, ${(err as any).stack}}`);
+        info.languageService.clearSourceMapperCache();
         res.status(500).send('Internal Server Error');
       }
     }
@@ -75,4 +82,27 @@ function createApp(options: { info: tsServer.PluginCreateInfo }) {
   });
 
   return app;
+}
+
+function findNode(nodeList: Node[], position: number) {
+  let result: Node | undefined;
+  for (const node of nodeList) {
+    if (node.pos > position) {
+      continue;
+    }
+    if (node.end < position) {
+      continue;
+    }
+    const children = node.getChildren();
+    if (children.length === 0) {
+      result = node;
+      break;
+    }
+    const childResult = findNode(node.getChildren(), position);
+    if (childResult) {
+      result = childResult;
+      break;
+    }
+  }
+  return result;
 }
