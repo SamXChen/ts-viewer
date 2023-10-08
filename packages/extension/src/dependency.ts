f979747160fd8c2514f833ad2163272a3f6372ad
samxchen <samxchen@tencent.com>
2023-10-08 10:54:54 +0800

    init

--- packages/extension/src/dependency.ts
+++ packages/extension/src/dependency.ts
@@ -1,23 +1,56 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
 import * as cp from 'child_process';
+
+const logger = vscode.window.createOutputChannel('ts-viewer');
 
 export async function installDependencies(context: vscode.ExtensionContext) {
   const extensionPath = context.extension.extensionPath;
-
-  const logger = vscode.window.createOutputChannel('ts-viewer');
-  logger.show();
   logger.appendLine('Extension path: ' + extensionPath);
 
-  const nodeModulesPath = path.join(extensionPath, 'node_modules');
-  if (fs.existsSync(nodeModulesPath)) {
+  if (await detectDependencies(context)) {
     logger.appendLine('Dependencies already installed.');
     return;
   }
   logger.appendLine('Installing dependencies...');
+  vscode.window.showInformationMessage('[Ts-Viewer-Extension] Installing dependencies...');
 
-  await new Promise((resolve, reject) => {
+  await implInstallDependencies(context);
+  logger.appendLine('Dependencies installed.');
+
+  vscode.window
+    .showInformationMessage('[Ts-Viewer-Extension] Dependencies installed.', 'Reload')
+    .then((value) => {
+      if (value === 'Reload') {
+        vscode.commands.executeCommand('workbench.action.reloadWindow');
+      }
+    });
+}
+
+function detectDependencies(context: vscode.ExtensionContext) {
+  const extensionPath = context.extension.extensionPath;
+  return new Promise((resolve) => {
+    cp.exec(
+      'npm ls --prod --silent --json',
+      {
+        cwd: extensionPath,
+      },
+      (err, stdout, stderr) => {
+        if (err) {
+          logger.appendLine(err.message);
+          resolve(false);
+          return;
+        }
+        logger.appendLine(stdout);
+        logger.appendLine(stderr);
+        resolve(true);
+      },
+    );
+  });
+}
+
+function implInstallDependencies(context: vscode.ExtensionContext) {
+  const extensionPath = context.extension.extensionPath;
+  return new Promise((resolve, reject) => {
     cp.exec(
       'npm install --production',
       {
@@ -35,14 +68,4 @@ export async function installDependencies(context: vscode.ExtensionContext) {
       },
     );
   });
-
-  logger.appendLine('Dependencies installed.');
-
-  vscode.window
-    .showInformationMessage('[Ts-Viewer-Extension] Dependencies installed.', 'Reload')
-    .then((value) => {
-      if (value === 'Reload') {
-        vscode.commands.executeCommand('workbench.action.reloadWindow');
-      }
-    });
 }
